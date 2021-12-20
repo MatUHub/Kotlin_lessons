e912bb76cae7745141a7294aa0df10cdd2076feb
MatU <radalert@mail.ru>
2021-12-20 22:00:12 +0300

    [Lesson 1] Создание Button ( с активацией clickListener ) и двух TextView

--- app/src/main/java/com/example/kotlin_lessons/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_lessons/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.kotlin_lessons
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Toast.makeText(this@MainActivity, "text", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }

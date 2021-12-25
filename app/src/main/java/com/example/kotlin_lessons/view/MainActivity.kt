9a1070cabbf97dc59827ba44ceba81b0d95b2566
MatU <radalert@mail.ru>
2021-12-25 13:38:05 +0300

    [Lesson 2] Создана структура model-view-view_model

--- app/src/main/java/com/example/kotlin_lessons/view/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_lessons/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.kotlin_lessons
+package com.example.kotlin_lessons.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kotlin_lessons.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

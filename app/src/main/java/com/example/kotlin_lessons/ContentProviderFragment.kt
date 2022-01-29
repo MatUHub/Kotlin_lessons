6a5b807e929694109fbdc8c17ea6a3ca94d85a19
MatU <mattves@bk.ru>
2022-01-29 14:25:40 +0300

    [Lesson 9] создан файл ContentProviderFragment для получения данных записанных в приложении контактов в отдельный список

--- app/src/main/java/com/example/kotlin_lessons/ContentProviderFragment.kt
+++ app/src/main/java/com/example/kotlin_lessons/ContentProviderFragment.kt
@@ -4,11 +4,13 @@ import android.Manifest
 import android.app.AlertDialog
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.example.kotlin_lessons.databinding.FragmentContentProviderBinding
 
@@ -81,7 +83,32 @@ class ContentProviderFragment : Fragment() {
     }
 
     private fun getContacts() {
-
+context?.let {
+    // сущность стоящая над всеми базами данных
+    val contentResolver = it.contentResolver
+    //создаем запрос в базу данных
+    val cursor = contentResolver.query(
+        ContactsContract.Contacts.CONTENT_URI,
+        null,
+        null,
+        null,
+        ContactsContract.Contacts.DISPLAY_NAME + " ASC"
+    )
+    cursor?.let { cursor ->
+        for (i in 5 .. 10){
+            cursor.moveToPosition(i)
+            val name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+            addView(name)
+        }
+    }
+    cursor?.close()
+}
+    }
+    private fun addView(name : String){
+        binding.containerForContacts.addView(TextView(requireContext()).apply {
+            text = name
+            textSize = 30f
+        })
     }
 
     private fun showDialog() {

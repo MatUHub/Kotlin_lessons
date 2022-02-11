10fb47dff67a829571f6423abc1ffee51af6c94f
MatU <mattves@bk.ru>
2022-02-11 16:57:31 +0300

    [Lesson 12] заменили requestPermission на registerForActivityResult

--- app/src/main/java/com/example/kotlin_lessons/view_model/ContentProviderFragment.kt
+++ app/src/main/java/com/example/kotlin_lessons/view_model/ContentProviderFragment.kt
@@ -10,6 +10,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.kotlin_lessons.databinding.FragmentContentProviderBinding
@@ -60,10 +61,19 @@ class ContentProviderFragment : Fragment() {
             }
         }
     }
+    val launcher = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            it -> if(it){
+        getContacts()
+    } else{
 
+    }
+    }
     val REQUEST_CODE = 123
     private fun myRequestPermission() {
-        requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS), REQUEST_CODE)
+        //requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS), REQUEST_CODE)
+
+
+        launcher.launch(Manifest.permission.READ_CONTACTS)
     }
 
     override fun onRequestPermissionsResult(
